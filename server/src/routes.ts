139751fe74f58b74dc1b416c2229324a91074e85
Paulo Rafael Barros de Aguiar <108704252+paulorbaguiar@users.noreply.github.com>
2023-12-06 12:21:33 -0300

    feat: add control identity and routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "./controllers/UserController";
+import ServiceController from "./controllers/ServiceController";
 
 const routes = express.Router();
 
@@ -7,5 +8,12 @@ routes.post("/user", userController.create);
 routes.get("/user", userController.get);
 routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
+
+
+// rotas ServiceController
+routes.post("/service", ServiceController.create)
+routes.get("/service", ServiceController.get);
+routes.delete("/service/:id", ServiceController.delete);
+routes.patch("/service/:id", ServiceController.update);
 
 export default routes;
