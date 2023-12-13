96d0bb3fb22b6fcebf291b151cc6fb1c5f6dc51f
Paulo Rafael Barros de Aguiar <108704252+paulorbaguiar@users.noreply.github.com>
2023-12-13 10:21:17 -0300

    ServiceController

---
+++ client/src/components/AboutCard/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AboutCard = () => {
+
+};
+
+export default AboutCard;
---
+++ server/prisma/migrations/20231207140640_service/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Service" (
+    "id" SERIAL NOT NULL,
+    "imageLink" TEXT NOT NULL,
+    "serviceDescription" TEXT NOT NULL,
+
+    CONSTRAINT "Service_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Test" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+
+    CONSTRAINT "Test_pkey" PRIMARY KEY ("id")
+);
