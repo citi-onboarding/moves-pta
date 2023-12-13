83ff303f5f2b28b6a16ba4ae9b3308e1374b6fc6
Paulo Rafael Barros de Aguiar <108704252+paulorbaguiar@users.noreply.github.com>
2023-12-13 10:53:26 -0300

    feat/ CRUDservice (#2)
    * feat: add control identity and routes
    
    * Create ServiceController.ts

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "./controllers/UserController";
+import ServiceController from "./controllers/ServiceController";
 import personController from "./controllers/PersonController";
 
 const routes = express.Router();
@@ -8,6 +9,12 @@ routes.post("/user", userController.create);
 routes.get("/user", userController.get);
 routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
+
+// rotas ServiceController
+routes.post("/service", ServiceController.create)
+routes.get("/service", ServiceController.get);
+routes.delete("/service/:id", ServiceController.delete);
+routes.patch("/service/:id", ServiceController.update);
 
 routes.post("/person", personController.create);
 routes.get("/person", personController.get);
